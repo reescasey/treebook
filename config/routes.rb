2dbc526434ce42cd6d7bf3a33788a175a02fbda5
Casey Rees <casey@podium.co>
2012-09-06 11:37:35 -0600

    Took away 'high voltage' gem file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Treebook::Application.routes.draw do
   devise_for :users
 
   resources :statuses
+  root to: 'statuses#index'
 
-  root :to=> 'statuses#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
